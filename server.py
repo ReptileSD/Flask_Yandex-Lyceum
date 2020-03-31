24e4d6968d43219cccfd3d55da2f06bd4cc94acf
ReptileSD <50035939+ReptileSD@users.noreply.github.com>
2020-03-31 15:28:56 +1000

    Update server.py

--- server.py
+++ server.py
@@ -37,7 +37,35 @@ def image_mars():
                      alt="здесь должна была быть картинка, но не нашлась">
                     <p>Это подпись под картинкой Марса</p>
                   </body>
-                </html>""".format()
+                </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet"
+                    href={url_for('static', filename='css/mystyle.css')}>
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='photos/mars.png')}"
+                     alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-dark" id="123">Человечество вырастает из детства.</div>
+                    <div class="alert alert-success">Человечеству мала одна планета.</div>
+                    <div class="alert alert-secondary">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                    <div class="alert alert-warning">И начнем с Марса!</div>
+                    <div class="alert alert-danger">Присоединяйся!</div>
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
